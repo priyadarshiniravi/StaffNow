0c69256a863281f983898f181114e77a634a147c
Priyadarshini Ravi <priyara@thoughtworks.com>
2018-02-17 00:18:11 +0530

    basic new project form and project layout - Priya

--- app/Component/Project.jsx
+++ app/Component/Project.jsx
@@ -1,14 +1,29 @@
 import React from 'react';
 import Header from './Header';
+import {NavLink, Card, CardTitle, CardText, Col, Row} from 'reactstrap';
+
 
 export default class Project extends React.Component {
 	render() {
 		return (
 			<div>
 				<Header />
-				hello Project!!
+				<div>
+					<Card body>
+						<Row>
+							<Col sm="6">
+								<CardTitle> 6 Projects </CardTitle>
+								<CardText> Project 1 </CardText>
+								<CardText> Project 2 </CardText>
+								<CardText> Project 3 </CardText>
+							</Col>
+							<Col sm="6">
+								<NavLink href="/new-project">Add Project</NavLink>
+							</Col>
+						</Row>
+					</Card>
+				</div>
 			</div>
-
 		);
 	}
 }
--- app/index.jsx
+++ app/index.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import {Provider} from 'react-redux';
 import ReactDOM from 'react-dom';
-import store from './redux/store';
+import {BrowserRouter, Route} from 'react-router-dom';
+
 import './styles/index.scss';
 import Dashboard from './Component/Dashboard';
 import Project from './Component/Project';
-import {BrowserRouter, Route} from 'react-router-dom';
+import ProjectForm from './Component/ProjectForm';
 
 
 ReactDOM.render(
@@ -13,6 +13,7 @@ ReactDOM.render(
 		<div>
 			<Route exact path="/" component={Dashboard} />
 			<Route path="/project" component={Project} />
+			<Route path="/new-project" component={ProjectForm} />
 		</div>
 	</BrowserRouter>,
 	document.getElementById('app'),
